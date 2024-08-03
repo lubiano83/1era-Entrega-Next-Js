7ebc687a2105880eb37e12b9967e17130e9ee28c
lubiano83 <lubiano83@gmail.com>
2024-08-03 19:31:48 -0400

    problema encontrado

--- app/hooks/usePrice.js
+++ app/hooks/usePrice.js
@@ -1,5 +1,3 @@
-"use client";
-
 export const usePrice = () => {
 
     try {
--- app/pages/cart/layout.js
+++ app/pages/cart/layout.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Banner from '@/app/components/Banner';
+import Banner from '../../components/Banner';
 
 const CartLayout = ({children}) => {
   return (
