69a3e60786eb3bb75ea5f45ebcc5d20b619a6cd2
lubiano83 <lubiano83@gmail.com>
2024-08-04 17:26:12 -0400

    agragando flecha al cart

--- app/pages/cart/layout.js
+++ app/pages/cart/layout.js
@@ -3,9 +3,9 @@ import Banner from '../../components/Banner';
 
 const CartLayout = ({children}) => {
   return (
-    <div className='bg-white h-full flex flex-col justify-between items-center'>
-        {children}
-        <Banner>Por compras antes de las 12 PM, el envio se gestionara el mismo dia...</Banner>
+    <div className='bg-white h-full flex flex-col justify-between items-center text-center'>
+      <Banner>Por compras antes de las 12 PM, el envio se gestionara el mismo dia...</Banner>
+      {children}
     </div>
   )
 }; export default CartLayout;
