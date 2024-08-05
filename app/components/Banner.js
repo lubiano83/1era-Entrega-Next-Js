fd32ddfe546c68a21dbfea320e7a4982e1ddc6e4
lubiano83 <lubiano83@gmail.com>
2024-08-05 18:13:09 -0400

    ajustes varios

--- app/components/Banner.js
+++ app/components/Banner.js
@@ -11,11 +11,11 @@ const Banner = ({children}) => {
 
   return (
     <div className={`${isDarkMode ? 'bg-orange-400' : 'bg-blue-400'} py-2 px-8 w-full text-white font-bold flex justify-between items-center`}>
-      <Image src={"/arrow-sm-left-svgrepo-com.svg"} alt={"autoshop arrowback"} height={45} onClick={() => router.back()} width={45} />
+      <Image src={"/arrow-sm-left-svgrepo-com.svg"} alt={"autoshop arrowback"} height={45} onClick={() => router.back()} width={45} className='cursor-pointer hover:scale-110'/>
       <div className='w-full flex justify-evenly items-center flex-wrap px-4'>
         {children}
       </div>
-      <Image src={"/arrow-sm-right-svgrepo-com.svg"} alt={"autoshop arrowfoward"} height={45} width={45} onClick={() => router.forward()}/>
+      <Image src={"/arrow-sm-right-svgrepo-com.svg"} alt={"autoshop arrowfoward"} height={45} width={45} onClick={() => router.forward()} className='cursor-pointer hover:scale-110'/>
     </div>
   )
 }; export default Banner;
--- app/page.js
+++ app/page.js
@@ -1,11 +1,6 @@
-"use client";
 import Main from "./components/Main";
-import NotFound from "./not-found";
-import { useParams } from "next/navigation";
 
 export default function Home() {
-
-  const { category, brand} = useParams();
 
   return (
     <div className="flex flex-col w-full justify-center items-center bg-white h-full text-gray-700 py-8">
