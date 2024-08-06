d9d6a4e9d1e68be13feb0e834ba70eabc2f9f53d
lubiano83 <lubiano83@gmail.com>
2024-08-05 22:18:06 -0400

    ajustes varios

--- app/pages/login/layout.js
+++ app/pages/login/layout.js
@@ -4,7 +4,7 @@ import Banner from '@/app/components/Banner';
 const LoginLayout = ({children}) => {
   return (
     <div className='bg-white w-full h-full flex flex-col justify-between items-center text-center'>
-        <Banner>OJO: Debe ingresar con su cuenta para poder comprar en la tienda!!</Banner>
+        <Banner>OJO: Debe ingresar con su cuenta para poder comprar en la tienda...</Banner>
       {children}
     </div>
   )
