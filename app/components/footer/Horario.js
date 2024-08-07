01f519bbfd8238a599f170d5fea367ae3fc8ff2d
lubiano83 <lubiano83@gmail.com>
2024-08-06 22:00:54 -0400

    ajustes varios

--- app/components/footer/Horario.js
+++ app/components/footer/Horario.js
@@ -4,7 +4,7 @@ const Horario = () => {
   return (
     <section className="px-4 text-center font-bold">
       <div>Horario de Atención:</div>
-      <div>Lunes a Viernes: 8:30 AM - 6:00 PM & Sabados: 8:30 - 14:30 PM</div>
+      <div>Lunes a Viernes: 8:30 - 18:00 & Sabados: 8:30 - 14:30</div>
       <div>Horario Continuado</div>
     </section>
   )
