82702a7ddb7eecf9027635e489c3809d319e5730
lubiano83 <lubiano83@gmail.com>
2024-08-08 18:52:16 -0400

    ajustes varios

--- app/pages/products/page.js
+++ app/pages/products/page.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import ProductsList from '../../components/products/ProductsList';
+import mockData from '../../data/mockData';
 
 const Shop = () => {
 
   return (
     <div className='w-full h-full flex'>
-     <ProductsList category={"todos"} />
+     <ProductsList category={"todos"} data={mockData} />
     </div>
   )
 }; export default Shop;
